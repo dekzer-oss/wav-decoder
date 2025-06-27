48fd9a6bd6c3c960ed77f176a412b73c090a5c72
Maikel Eckelboom <maikeleckelboom@outlook.com>
2025-06-27 23:29:39 +0200

    chore: Update GitHub workflow and tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,13 +2,19 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['cjs', 'esm'],
-  dts: true,
-  // splitting: false,
+  format: ['esm', 'cjs'],
+  dts: {
+    entry: 'src/index.ts',
+  },
   sourcemap: true,
   clean: true,
   tsconfig: 'tsconfig.build.json',
   minify: true,
+  outExtension({ format }) {
+    return {
+      js: format === 'esm' ? '.mjs' : '.js',
+    };
+  },
   terserOptions: {
     compress: {
       drop_console: true,
